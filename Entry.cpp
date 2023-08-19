099f4224fec385c37964e7c8e082b085ca362047
unknown <kostmetallist@gmail.com>
2023-08-20 01:47:20 +0300

    Add basic item class

---
+++ Entry.cpp
@@ -0,0 +1,6 @@
+#include "Entry.h"
+
+Entry::Entry(int key, int value) {
+	this->key = key;
+	this->value = value;
+}
---
+++ Entry.h
@@ -0,0 +1,8 @@
+#pragma once
+class Entry {
+public:
+	int key;
+	int value;
+
+	Entry(int, int);
+};
