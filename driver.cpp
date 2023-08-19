db2a581cf8b0aa08c1e9376b3d5e3fde670cc83b
unknown <kostmetallist@gmail.com>
2023-08-20 01:50:47 +0300

    Enable memleak reporting

--- driver.cpp
+++ driver.cpp
@@ -1,6 +1,15 @@
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
 #include <iostream>
+#include "Entry.h"
 
 int main()
 {
+    Entry *e1 = new Entry(1, 42);
     std::cout << "The interaction with the data structure is expected here.\n";
+    delete e1;
+
+    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+    _CrtDumpMemoryLeaks();
+    return 0;
 }
